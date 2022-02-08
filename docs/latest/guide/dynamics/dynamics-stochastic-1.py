7e43f8b6c5339461d14c6e877c04f5b45df50e87
Simon Cross <hodgestar@gmail.com>
2022-02-09 00:17:10 +0100

    Add 4.6.3 documentation.

--- docs/latest/guide/dynamics/dynamics-stochastic-1.py
+++ docs/latest/guide/dynamics/dynamics-stochastic-1.py
@@ -1,15 +1,37 @@
-times = np.linspace(0.0, 10.0, 201)
-psi0 = tensor(fock(2, 0), fock(10, 5))
-a  = tensor(qeye(2), destroy(10))
-sm = tensor(destroy(2), qeye(10))
+import numpy as np
+import matplotlib.pyplot as plt
+import qutip as qt
 
-H = 2*np.pi*a.dag()*a + 2*np.pi*sm.dag()*sm + 2*np.pi*0.25*(sm*a.dag() + sm.dag()*a)
-data = ssesolve(H, psi0, times, sc_ops=[np.sqrt(0.1) * a], e_ops=[a.dag()*a, sm.dag()*sm], method="homodyne")
+# parameters
+DIM = 20             # Hilbert space dimension
+DELTA = 5*2*np.pi    # cavity detuning
+KAPPA = 2            # cavity decay rate
+INTENSITY = 4        # intensity of initial state
+NUMBER_OF_TRAJECTORIES = 500
 
-plt.figure()
-plt.plot(times, data.expect[0], times, data.expect[1])
-plt.title('Homodyne time evolution')
-plt.xlabel('Time')
-plt.ylabel('Expectation values')
-plt.legend(("cavity photon number", "atom excitation probability"))
-plt.show()
+# operators
+a = qt.destroy(DIM)
+x = a + a.dag()
+H = DELTA*a.dag()* a
+
+rho_0 = qt.coherent(DIM, np.sqrt(INTENSITY))
+times = np.arange(0, 1, 0.0025)
+
+stoc_solution = qt.smesolve(H, rho_0, times,
+                            c_ops=[],
+                            sc_ops=[np.sqrt(KAPPA) * a],
+                            e_ops=[x],
+                            ntraj=NUMBER_OF_TRAJECTORIES,
+                            nsubsteps=2,
+                            store_measurement=True,
+                            dW_factors=[1],
+                            method='homodyne')
+
+fig, ax = plt.subplots()
+ax.set_title('Stochastic Master Equation - Homodyne Detection')
+ax.plot(times, np.array(stoc_solution.measurement).mean(axis=0)[:].real,
+        'r', lw=2, label=r'$J_x$')
+ax.plot(times, stoc_solution.expect[0], 'k', lw=2,
+        label=r'$\langle x \rangle$')
+ax.set_xlabel('Time')
+ax.legend()
--- docs/latest/guide/guide-visualization-7.py
+++ docs/latest/guide/guide-visualization-7.py
@@ -1,21 +1,11 @@
 Q_coherent = qfunc(rho_coherent, xvec, xvec)
-
 Q_thermal = qfunc(rho_thermal, xvec, xvec)
-
 Q_fock = qfunc(rho_fock, xvec, xvec)
-
 fig, axes = plt.subplots(1, 3, figsize=(12,3))
-
 cont0 = axes[0].contourf(xvec, xvec, Q_coherent, 100)
-
 lbl0 = axes[0].set_title("Coherent state")
-
 cont1 = axes[1].contourf(xvec, xvec, Q_thermal, 100)
-
 lbl1 = axes[1].set_title("Thermal state")
-
 cont0 = axes[2].contourf(xvec, xvec, Q_fock, 100)
-
 lbl2 = axes[2].set_title("Fock state")
-
 plt.show()
