0715c403b4aaea28c15c20638d7d827da40726ef
Maximilian Meyer-Mölleringhof <mixmax199989@gmail.com>
2025-04-15 14:41:01 +0900

    Supress element is null error

--- js/show_more.js
+++ js/show_more.js
@@ -1,4 +1,7 @@
-document.getElementById('show-more-btn').addEventListener('click', function () {
-    const collapseElement = document.getElementById('show-more-btn');
-    this.textContent = collapseElement.classList.contains('collapsed') ? 'Show More' : 'Show Less';
-});
+showMoreElement = document.getElementById('show-more-btn');
+if (showMoreElement) {
+    showMoreElement.addEventListener('click', function () {
+        const collapseElement = document.getElementById('show-more-btn');
+        this.textContent = collapseElement.classList.contains('collapsed') ? 'Show More' : 'Show Less';
+    });
+}
